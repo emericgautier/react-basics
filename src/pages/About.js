016b86500d4f4378b14faafdb2c94a222064b2f2
emericgautier <emericgtr@gmail.com>
2022-08-24 12:49:06 +0200

    Create pages

---
+++ src/pages/About.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>A PROPOS</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa obcaecati
+        quibusdam, dolor totam velit nostrum. Facere, voluptate ducimus harum
+        exercitationem hic sed amet officia, possimus placeat, recusandae nihil
+        libero. Accusamus esse sint reiciendis expedita ipsa tempore quam?
+        Mollitia ab tenetur, aliquam a eum, sint sequi cum vel corporis, ex
+        eaque officia iure nemo ea. Maxime aut, rem in, eos iste, quia impedit
+        optio quisquam quo ab ut temporibus vitae possimus minus nulla
+        laudantium beatae. Dignissimos provident aspernatur assumenda enim
+        doloribus, magnam perferendis, veritatis error autem hic voluptas ipsam
+        quidem impedit reprehenderit corrupti maxime tenetur magni omnis. Vel
+        dolor doloribus dicta?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
+        blanditiis. Iure, non quos, eveniet a repudiandae, totam perferendis
+        asperiores repellendus neque sapiente iste voluptatem qui nulla minima
+        illo. Error dolore perferendis nobis cumque dolorum possimus nulla eius
+        eos, quas molestias, obcaecati, adipisci facere id earum voluptate?
+        Dolorem veritatis amet quisquam natus placeat suscipit non minima
+        molestiae eaque voluptates, odit saepe vitae inventore beatae, iusto
+        autem deleniti aut iure at. Hic neque quibusdam illum deleniti odio est?
+        Impedit libero deleniti neque labore! Dolore dolores blanditiis impedit
+        dolorum officiis, ipsam natus, explicabo possimus ducimus aliquid
+        perferendis obcaecati tempora repudiandae vero sint. Aliquid unde
+        debitis possimus, ullam reprehenderit rem enim, exercitationem quas
+        totam beatae, in laudantium numquam reiciendis! Nihil debitis nisi
+        doloremque incidunt quis quisquam sint adipisci eaque! Vero, eius, cum
+        eligendi illo quisquam similique molestiae quo, cumque laudantium dicta
+        debitis doloremque iure placeat molestias ratione commodi obcaecati
+        architecto quas. Obcaecati vel doloribus odio quo repudiandae natus
+        culpa aspernatur fuga ipsam, sunt sint corrupti eveniet esse, rerum eum
+        nobis nisi animi corporis alias quia facere laborum, omnis laudantium
+        magni! Deleniti beatae atque nisi inventore, quod fugiat aut id optio
+        praesentium quia aspernatur fugit suscipit. Dolore aliquid alias error
+        dolorem autem molestiae sed neque.
+      </p>
+    </div>
+  );
+};
+
+export default About;
